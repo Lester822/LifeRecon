0bc4880c97d687384b71197234930093b2d24df6
Michael S <mstang822@gmail.com>
2024-04-09 08:49:15 -0500

    Initial Commit

---
+++ LifeRecon/LifeReconApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeReconApp.swift
+//  LifeRecon
+//
+//  Created by Michael Stang on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LifeReconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
